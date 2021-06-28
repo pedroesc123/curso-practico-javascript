11042f7d76c10a922475da9c3bb02abc3a1fc9c5
Pedro Espinoza <pedro.espinoza.c@uni.pe>
2021-06-28 12:08:00 -0500

    Agregando las funciones

--- figuras.js
+++ figuras.js
@@ -1,1 +1,51 @@
-console.log("Hello, World!");
+// Código del cuadrado
+console.group("Cuadrados");
+// const ladoCuadrado = 5;
+// console.log(`Los lados del cuadrado miden: ${ladoCuadrado}cm.`);
+
+function perimetroCuadrado(lado){
+    return lado*4;
+}
+
+
+function areaCuadrado(lado) {
+    return lado * lado;
+}
+
+console.groupEnd();
+
+
+//Triángulo
+console.group("Triángulo");
+
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2+ base;
+}
+
+function areaTriangulo(base, altura){
+    return (base * altura) / 2;
+} 
+
+console.groupEnd();
+
+
+//Código del círculo
+console.group('Círculos');
+
+function diametroCirculo(radio) {
+    return radio * 2;
+}
+
+//Pi
+const pi = (Math.PI).toFixed(3);
+
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio);
+    return pi * diametro;
+}
+
+function areaCirculo(radio) {
+    return pi * radio * radio;
+}
+
+console.groupEnd();
