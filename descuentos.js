79e4d707d0d42f0641a570606d5a49826080a30b
Pedro Espinoza Cordero <pedro.espinoza.c@uni.pe>
2021-07-05 14:14:49 -0500

    Finalizado el problema de descuentos

--- descuentos.js
+++ descuentos.js
@@ -17,3 +17,23 @@ function calcularPrecioConDescuento(precio, descuento){
 
     return precioConDescuento;
 }
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = Number(inputPrice.value);
+    const inputDiscount = document.getElementById("InputDiscount");
+    const valueDiscount = inputDiscount.value;
+
+    if (valueDiscount == "Cupon1") {
+        descuento = 15;
+    } else if (valueDiscount == "Cupon2") {
+        descuento = 25;
+    } else {
+        descuento = 35;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento es: $" + precioConDescuento;
+}
